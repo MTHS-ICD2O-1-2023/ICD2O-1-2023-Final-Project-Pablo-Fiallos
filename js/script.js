cfa559050db7ead610e0b34e251dad882b0f959a
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-06-05 18:50:42 +0000

    formating fix

--- js/script.js
+++ js/script.js
@@ -5,21 +5,3 @@
 // This file contains the JS functions for index.html
 
 "use strict"
-
-function updateCookieCount () {
-
-  if (localStorage.cookies) {
-    document.getElementById("user-info").innerHTML = "Cookies: " + localStorage.cookies
-  } else {
-    document.getElementById("user-info").innerHTML = "Cookies: 0"
-  }
-}
-
-function cookieClicked() {
-  if (localStorage.cookies) {
-    localStorage.cookies = Number(localStorage.cookies) + 1 
-  } else {
-    localStorage.cookies = 1
-  }
-  document.getElementById("user-info").innerHTML = "Cookies: " + localStorage.cookies
-}
