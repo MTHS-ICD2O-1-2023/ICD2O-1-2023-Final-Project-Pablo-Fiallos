aa36fcf6a228db2a270817f41259c43ada3d7497
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-06-10 17:48:18 +0000

    test

--- js/script.js
+++ js/script.js
@@ -47,6 +47,7 @@ function enterClicked() {
     moneyDifference <= Number(localStorage.money) &&
     stocksSoldOrBought >= -1 * Number(localStorage.stocksOwned)
   ) {
+    document.getElementById("message-label").innerHTML = ""
     localStorage.money = (Number(localStorage.money) - moneyDifference).toFixed(
       2
     )
@@ -64,7 +65,100 @@ function enterClicked() {
     }
     localStorage.day = Number(localStorage.day) + 1
     update()
+    sendMessage()
   } else {
-    console.log("error")
+    document.getElementById("message-label").innerHTML =
+      "Impossible transaction. Try again."
+  }
+}
+
+function sendMessage() {
+  if (Number(localStorage.day) >= 100 && Number(localStorage.day) <= 130) {
+    document.getElementById("message-label").innerHTML =
+      "good luck, it's gonna be a while..."
+  } else if (
+    Number(localStorage.day) >= 200 &&
+    Number(localStorage.day) <= 230
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "you better make it to the end. it will be worth it. it will be fun."
+  } else if (
+    Number(localStorage.day) >= 300 &&
+    Number(localStorage.day) <= 330
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "yes, in order to win you must multiply your money by one thousand."
+  } else if (
+    Number(localStorage.day) >= 400 &&
+    Number(localStorage.day) <= 430
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "tip: you can put 0 into the thing and skip days. hopefully you've already discovered this..."
+  } else if (
+    Number(localStorage.day) >= 500 &&
+    Number(localStorage.day) <= 530
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "patience is key. in this game and in real life."
+  } else if (
+    Number(localStorage.day) >= 600 &&
+    Number(localStorage.day) <= 630
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "is there any point to winning this game? yes there is. you will be proud of yourself. and i will be proud of you."
+  } else if (
+    Number(localStorage.day) >= 700 &&
+    Number(localStorage.day) <= 730
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "the more you struggle, the stronger you become. the stronger you become, the less you must struggle."
+  } else if (
+    Number(localStorage.day) >= 800 &&
+    Number(localStorage.day) <= 830
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "and this game is but a constant struggle, so it will make you happier in the long-term."
+  } else if (
+    Number(localStorage.day) >= 900 &&
+    Number(localStorage.day) <= 930
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "most games give gratification; so, they make you sadder in the long-term, for gratification is the enemy of struggle."
+  } else if (
+    Number(localStorage.day) >= 1000 &&
+    Number(localStorage.day) <= 1030
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "the sadder you are, the more desperately you seek gratification."
+  } else if (
+    Number(localStorage.day) >= 1100 &&
+    Number(localStorage.day) <= 1130
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "thus, you enter the cycle of addiction— this is exactly what the video game companies want!"
+  } else if (
+    Number(localStorage.day) >= 1200 &&
+    Number(localStorage.day) <= 1230
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "hence why they say video games rot your brain..."
+  } else if (
+    Number(localStorage.day) >= 1300 &&
+    Number(localStorage.day) <= 1330
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "but this game isn't addictive, thus it won't \"rot your brain.\""
+  } else if (
+    Number(localStorage.day) >= 1400 &&
+    Number(localStorage.day) <= 1430
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "if you at it that way, perhaps this is one of the best games you've ever played?"
+  } else if (
+    Number(localStorage.day) >= 1500 &&
+    Number(localStorage.day) <= 1530
+  ) {
+    document.getElementById("message-label").innerHTML =
+      "just a shower thought..."
   }
 }
